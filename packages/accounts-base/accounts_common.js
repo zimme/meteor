6639e819050a07692d12c0bb96c317f1f3a35b7a
Ben Newman <ben@meteor.com>
2015-04-16 18:01:21 -0400

    Implement a reusable AccountsCommon constructor.
    This involves moving Accounts.* methods defined in accounts_common.js to
    AccountsCommon.prototype.*.

--- packages/accounts-base/accounts_common.js
+++ packages/accounts-base/accounts_common.js
@@ -1,12 +1,49 @@
-/**
- * @namespace Accounts
- * @summary The namespace for all accounts-related methods.
- */
-Accounts = {};
+AccountsCommon = function AccountsCommon(ddpUrl) {
+  // Currently this is read directly by packages like accounts-password
+  // and accounts-ui-unstyled.
+  this._options = {};
 
-// Currently this is read directly by packages like accounts-password
-// and accounts-ui-unstyled.
-Accounts._options = {};
+  this._initConnection(ddpUrl);
+
+  this._users = new Mongo.Collection("users", {
+    // There is an allow call in accounts_server that restricts this
+    // collection.
+    _preventAutopublish: true,
+    connection: this.connection
+  });
+
+  this._initLoginHooks();
+};
+
+var Ap = AccountsCommon.prototype;
+
+/**
+ * @summary Get the current user id, or `null` if no user is logged in. A reactive data source.
+ * @locus Anywhere but publish functions
+ */
+Ap.userId = function () {
+  throw new Error("userId method not implemented");
+};
+
+Ap.user = function () {
+  var userId = this.userId();
+  return userId ? this._users.findOne(userId) : null;
+};
+
+// Note that Accounts is defined separately in accounts_client.js and
+// accounts_server.js.
+
+Meteor.userId = function () {
+  return Accounts.userId();
+};
+
+/**
+ * @summary Get the current user record, or `null` if no user is logged in. A reactive data source.
+ * @locus Anywhere but publish functions
+ */
+Meteor.user = function () {
+  return Accounts.user();
+};
 
 // how long (in days) until a login token expires
 var DEFAULT_LOGIN_EXPIRATION_DAYS = 90;
@@ -52,7 +89,7 @@ CONNECTION_CLOSE_DELAY_MS = 10 * 1000;
  * @param {Number} options.loginExpirationInDays The number of days from when a user logs in until their token expires and they are logged out. Defaults to 90. Set to `null` to disable login expiration.
  * @param {String} options.oauthSecretKey When using the `oauth-encryption` package, the 16 byte key using to encrypt sensitive account credentials in the database, encoded in base64.  This option may only be specifed on the server.  See packages/oauth-encryption/README.md for details.
  */
-Accounts.config = function(options) {
+Ap.config = function (options) {
   // We don't want users to accidentally only call Accounts.config on the
   // client, where some of the options will have partial effects (eg removing
   // the "create account" button from accounts-ui if forbidClientAccountCreation
@@ -91,21 +128,19 @@ Accounts.config = function(options) {
   // set values in Accounts._options
   _.each(VALID_KEYS, function (key) {
     if (key in options) {
-      if (key in Accounts._options) {
+      if (key in this._options) {
         throw new Error("Can't set `" + key + "` more than once");
-      } else {
-        Accounts._options[key] = options[key];
       }
+      this._options[key] = options[key];
     }
-  });
-
-  // If the user set loginExpirationInDays to null, then we need to clear the
-  // timer that periodically expires tokens.
-  if (Meteor.isServer)
-    maybeStopExpireTokensInterval();
+  }, this);
 };
 
-if (Meteor.isClient) {
+Ap._initConnection = function (url) {
+  if (! Meteor.isClient) {
+    return;
+  }
+
   // The connection used by the Accounts system. This is the connection
   // that will get logged in by Meteor.login(), and this is the
   // connection whose login state will be reflected by Meteor.userId().
@@ -113,10 +148,11 @@ if (Meteor.isClient) {
   // It would be much preferable for this to be in accounts_client.js,
   // but it has to be here because it's needed to create the
   // Meteor.users collection.
-  Accounts.connection = Meteor.connection;
 
-  if (typeof __meteor_runtime_config__ !== "undefined" &&
-      __meteor_runtime_config__.ACCOUNTS_CONNECTION_URL) {
+  if (url) {
+    this.connection = DDP.connect(url);
+  } else if (typeof __meteor_runtime_config__ !== "undefined" &&
+             __meteor_runtime_config__.ACCOUNTS_CONNECTION_URL) {
     // Temporary, internal hook to allow the server to point the client
     // to a different authentication server. This is for a very
     // particular use case that comes up when implementing a oauth
@@ -124,83 +160,73 @@ if (Meteor.isClient) {
     //
     // We will eventually provide a general way to use account-base
     // against any DDP connection, not just one special one.
-    Accounts.connection = DDP.connect(
-      __meteor_runtime_config__.ACCOUNTS_CONNECTION_URL)
+    this.connection =
+      DDP.connect(__meteor_runtime_config__.ACCOUNTS_CONNECTION_URL);
+  } else {
+    this.connection = Meteor.connection;
   }
-}
-
-// Users table. Don't use the normal autopublish, since we want to hide
-// some fields. Code to autopublish this is in accounts_server.js.
-// XXX Allow users to configure this collection name.
-
-/**
- * @summary A [Mongo.Collection](#collections) containing user documents.
- * @locus Anywhere
- * @type {Mongo.Collection}
- */
-Meteor.users = new Mongo.Collection("users", {
-  _preventAutopublish: true,
-  connection: Meteor.isClient ? Accounts.connection : Meteor.connection
-});
-// There is an allow call in accounts_server that restricts this
-// collection.
+};
 
 // loginServiceConfiguration and ConfigError are maintained for backwards compatibility
 Meteor.startup(function () {
   var ServiceConfiguration =
     Package['service-configuration'].ServiceConfiguration;
-  Accounts.loginServiceConfiguration = ServiceConfiguration.configurations;
-  Accounts.ConfigError = ServiceConfiguration.ConfigError;
+  Ap.loginServiceConfiguration = ServiceConfiguration.configurations;
+  Ap.ConfigError = ServiceConfiguration.ConfigError;
 });
 
 // Thrown when the user cancels the login process (eg, closes an oauth
 // popup, declines retina scan, etc)
-Accounts.LoginCancelledError = function(description) {
+Ap.LoginCancelledError = function (description) {
+  Error.apply(this, arguments);
   this.message = description;
 };
 
 // This is used to transmit specific subclass errors over the wire. We should
 // come up with a more generic way to do this (eg, with some sort of symbolic
 // error code rather than a number).
-Accounts.LoginCancelledError.numericError = 0x8acdc2f;
-Accounts.LoginCancelledError.prototype = new Error();
-Accounts.LoginCancelledError.prototype.name = 'Accounts.LoginCancelledError';
+Ap.LoginCancelledError.numericError = 0x8acdc2f;
+var LCEp = Ap.LoginCancelledError.prototype = Object.create(Error.prototype);
+LCEp.constructor = Ap.LoginCancelledError;
+LCEp.name = 'Accounts.LoginCancelledError';
 
-getTokenLifetimeMs = function () {
-  return (Accounts._options.loginExpirationInDays ||
+Ap.getTokenLifetimeMs = function () {
+  return (this._options.loginExpirationInDays ||
           DEFAULT_LOGIN_EXPIRATION_DAYS) * 24 * 60 * 60 * 1000;
 };
 
-Accounts._tokenExpiration = function (when) {
+Ap._tokenExpiration = function (when) {
   // We pass when through the Date constructor for backwards compatibility;
   // `when` used to be a number.
-  return new Date((new Date(when)).getTime() + getTokenLifetimeMs());
+  return new Date((new Date(when)).getTime() + this.getTokenLifetimeMs());
 };
 
-Accounts._tokenExpiresSoon = function (when) {
-  var minLifetimeMs = .1 * getTokenLifetimeMs();
+Ap._tokenExpiresSoon = function (when) {
+  var minLifetimeMs = .1 * this.getTokenLifetimeMs();
   var minLifetimeCapMs = MIN_TOKEN_LIFETIME_CAP_SECS * 1000;
   if (minLifetimeMs > minLifetimeCapMs)
     minLifetimeMs = minLifetimeCapMs;
   return new Date() > (new Date(when) - minLifetimeMs);
 };
 
-// Callback exceptions are printed with Meteor._debug and ignored.
-onLoginHook = new Hook({
-  debugPrintExceptions: "onLogin callback"
-});
-onLoginFailureHook = new Hook({
-  debugPrintExceptions: "onLoginFailure callback"
-});
+Ap._initLoginHooks = function () {
+  // Callback exceptions are printed with Meteor._debug and ignored.
+  this._onLoginHook = new Hook({
+    debugPrintExceptions: "onLogin callback"
+  });
 
+  this._onLoginFailureHook = new Hook({
+    debugPrintExceptions: "onLoginFailure callback"
+  });
+};
 
 /**
  * @summary Register a callback to be called after a login attempt succeeds.
  * @locus Anywhere
  * @param {Function} func The callback to be called when login is successful.
  */
-Accounts.onLogin = function (func) {
-  return onLoginHook.register(func);
+Ap.onLogin = function (func) {
+  return this._onLoginHook.register(func);
 };
 
 /**
@@ -208,6 +234,6 @@ Accounts.onLogin = function (func) {
  * @locus Anywhere
  * @param {Function} func The callback to be called after the login has failed.
  */
-Accounts.onLoginFailure = function (func) {
-  return onLoginFailureHook.register(func);
+Ap.onLoginFailure = function (func) {
+  return this._onLoginFailureHook.register(func);
 };
